32c0f222af42b11eeb46fff8ee6ceada110c6b37
Tassos Bareiss <tassosbareiss@gmail.com>
2025-03-07 20:41:25 -0700

    Create data_syncs table

--- db/migrate/20250308025102_create_data_syncs.rb
+++ db/migrate/20250308025102_create_data_syncs.rb
@@ -2,6 +2,9 @@ class CreateDataSyncs < ActiveRecord::Migration[8.0]
   def change
     create_table :data_syncs do |t|
       t.timestamps
+      t.datetime :started_at
+      t.datetime :completed_at
+      t.string :status, default: 'pending', null: false
     end
   end
 end
