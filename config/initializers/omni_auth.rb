36417e12376b0692fcf5334b04cc52373d00b816
Tassos Bareiss <tassosbareiss@gmail.com>
2025-03-11 21:43:29 -0600

    No OAuth in test environment

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -1,16 +1,16 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-           Rails.application.credentials.google.client_id,
-           Rails.application.credentials.google.client_secret,
-           scope: %w[
-             email
-             classroom.coursework.students.readonly
-             classroom.courses.readonly
-             classroom.rosters.readonly
-             classroom.student-submissions.students.readonly
-             classroom.topics.readonly
-           ],
-           prompt: "consent"
+unless Rails.env.test?
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :google_oauth2,
+            Rails.application.credentials.google.client_id,
+            Rails.application.credentials.google.client_secret,
+            scope: %w[
+              email
+              classroom.coursework.students.readonly
+              classroom.courses.readonly
+              classroom.rosters.readonly
+              classroom.student-submissions.students.readonly
+              classroom.topics.readonly
+            ],
+            prompt: "consent"
+  end
 end
-# Sage (STAM): 110499431832195285133
-# Sage (Precal): 110499431832195285133
